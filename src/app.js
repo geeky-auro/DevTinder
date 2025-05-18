adcc3fea1a2f29d90142d325e219de9da06311e4
geeky-auro <aurosaswat.apk@gmail.com>
2025-05-18 17:24:50 +0530

    Playing with HTTP Methods

--- src/app.js
+++ src/app.js
@@ -3,17 +3,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use("/", (req, res) => {
-  res.send("Hello Buddy :0");
-});
+// app.use("/", (req, res) => {
+//   res.send("Hello Buddy :0");
+// });
 
 app.use("/dashboard", (req, res) => {
-  res.send("Hello from the DashBoard!");
+  console.log(req.path);
+  res.send("Hello from the DashBoard!" + " " + req.path);
 });
 
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
 });
+
+app.get("/users", (req, res) => {
+  res.send({ firstName: "Auro", lastName: "Raj" });
+});
+
+
+app.post("/users", (req, res) => {
+    res.send("Data saved to the Database");
+  });
+
+
+  app.delete("/users", (req, res) => {
+    res.send("Data Deleted Successfully");
+  });
 
 app.listen(PORT, () => {
   // This will only be executed when our server has started successfully
